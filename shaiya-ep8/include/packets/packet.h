6fb74a0a3b777e55ec67b8c273af20ba355f6092
Eric <eric-dutra@hotmail.com>
2026-01-18 05:22:14 -0300

    feat(packets): implement packet handler

--- shaiya-ep8/include/packets/packet.h
+++ shaiya-ep8/include/packets/packet.h
@@ -1,10 +1,16 @@
+#pragma once
 #include <cstdint>
 
 namespace packets
 {
+    enum struct PacketType : uint16_t
+    {
+        AutoStatsList = 0x0120
+    };
+
 	struct Packet
 	{
-		uint16_t opcode;
+		PacketType opcode;
 		uint8_t data[512];
 	};
 }
