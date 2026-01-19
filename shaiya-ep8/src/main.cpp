6762b46d8bc7ac1f2e0834492c547ec6ce364f03
Eric <eric-dutra@hotmail.com>
2026-01-19 07:52:22 -0300

    feat(character-status): implement character_status packet

--- shaiya-ep8/src/main.cpp
+++ shaiya-ep8/src/main.cpp
@@ -2,12 +2,14 @@
 #include "include/packets/handler.h"
 #include "include/packets/character_list.h"
 #include "include/packets/character_details.h"
+#include "include/packets/character_status.h"
 
 void init()
 {
     packets::handler::hook();
     packets::character_list::hook();
     packets::character_details::hook();
+    packets::character_status::hook();
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
